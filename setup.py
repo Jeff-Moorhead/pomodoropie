f2586b1882a2cee38d827b4386e56e2a076418a8
Jeff Moorhead <jeff.moorhead1@gmail.com>
2020-05-19 18:35:26 -0400

    Rename project. Add LICENSE and README

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Pymodoro',
+    name='Pomodoropie',
     description='A Python pomodoro clock',
     author='Jeff Moorhead',
     author_email='jeff.moorhead1@gmail.com',
-    url='https://github.com/Jeff-Moorhead/pymodoro',
+    url='https://github.com/Jeff-Moorhead/pomodoropie',
     packages=find_packages(),
     entry_points = {
-        'console_scripts': ['pymodoro=pymodoro.__main__:main'],
+        'console_scripts': ['pomodoropie=pomodoropie.__main__:main'],
     }
 )
     
